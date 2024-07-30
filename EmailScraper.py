d0c49eb822cd3f77e700130232df81d174b08b33
hughu123 <hughu123@student.liu.se>
2024-07-30 18:19:56 +0200

    code cleanup, more promps added and version is updated that now displays a message from a .txt file

--- EmailScraper.py
+++ EmailScraper.py
@@ -1,20 +1,22 @@
 # Hugo Humble
-# 2024-07-28
-# Version 1.1
-
+# 2024-07-30
+# Version 1.2
 
 
     ##################
     # ATT GÖRA LISTA #
     ##################
     # 
-    # [] Skapa fler popup-rutor för "informativ" användning
+    # [75%] Skapa fler popup-rutor för "informativ" användning
     #
-    # [] Förenkla kod struktur med definerade funktioner istället
+    # [50%] Förenkla kod struktur med definerade funktioner istället
     #
     # [] Sortera email i två grupper, Guld och Silver-förmåner
     #
+    # [] Skapa en csv fil med mailadresserna
+    #       [] Välja att skapa listor med olika filter (som guld och silver separat)
     #
+    # []  
     #
 
 ###################
@@ -22,7 +24,7 @@
 ###################
 
 # pip install beautifulsoup4
-from bs4 import BeautifulSoup 
+from bs4 import BeautifulSoup # kanske inte ens behövs! (kolla vidare på detta...)
 
 # pip install selenium
 from selenium import webdriver 
@@ -35,64 +37,77 @@ from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
 import time
 import csv as csv
+
+# pip install easygui
 import easygui as eg
+
+
 
 #############
 # Functions #
 #############
 
 def login_sequence():
-
-    eg.exceptionbox("Make sure that you enter your login information correct, no \"try/catch\" has been implemented for incorrect login as of yet","Crucial message for End-User")
-    
-    username = ""
-    password = ""
+    message = load_text()
+    # print(message) # debugging
+    eg.exceptionbox(message,"Before using the program")
 
     driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
     
+    login_test(driver)
+
+    return driver
+
+def load_text():
+    text_file = ""
+    with open("message.txt","r") as f:
+        text_file = f.read()
+        f.close()
+    
+    return text_file
+
+
+def login_test(driver):
+    print("Starting login_test()") # debugging
+
+    # Gå till Trappas hemsida
     driver.get("https://personal.trappan.nu/login.php")
 
-    username_field = driver.find_element(By.NAME, "user_name")
-    password_field = driver.find_element(By.NAME, "password")
     
-    while(True):
+
+    while True:
+        # Hitta fält
+        username_field = driver.find_element(By.NAME, "user_name")
+        password_field = driver.find_element(By.NAME, "password")
+        # Försök logga in på hemsidan med angivna uppgifter
         username = eg.enterbox("Enter username: ", "Login")
         username_field.send_keys(username)
         password = eg.passwordbox("Enter password: ", "Login")  
-        password_field.send_keys(password) 
-        if username == "":
-            eg.exceptionbox("You didn't enter a username, please try again.","Failed to enter username")
-            continue
-        elif  password == "":
-            eg.exceptionbox("You didn't enter a password, please try again.","Failed to enter password")
-            continue
+        password_field.send_keys(password)
+
+        login_button = driver.find_element(By.NAME, "log_in")
+        login_button.click()
+
+        # Kontrollera inloggningsuppgifter
+        if driver.current_url == "https://personal.trappan.nu/login.php":
+                print("Loggin failed")
+                eg.exceptionbox("Ditt användarnamn eller lösenord var fel, vänligen försök igen!")
+                continue
         else:
-            # add case if login fails
-            # try:
-                login_button = driver.find_element(By.NAME, "log_in")
-                login_button.click()
+             break
+    print("login_test() ran successfully") # debugging
 
-                
-            
-            # finally:
-                break
+    
 
-    return username, password, driver
+
 
 try:
 
     ##################
     # Login Sequence #
     ##################
-    
-    # print("Before fetch_email()") # debugging
 
-    username, password, driver = login_sequence()
-
-    # Ensure login success
-    time.sleep(1)
-
-    # print("After fetch_email()") # debugging
+    driver = login_sequence()
 
     #####################
     # Benefits Sequence #
@@ -101,17 +116,12 @@ try:
     # driver.get("https://personal.trappan.nu/index.php?page=periodBenefits&period=105")
     driver.get("https://personal.trappan.nu/index.php?page=periodBenefits&period=106")
 
-    #Hämtar sidans källkod
-    
+    # Hämtar sidans källkod
     soup = BeautifulSoup(driver.page_source, "html.parser")
     
     # Filtrerar personalens url till inviduella profiler
     tbody = soup.find("tbody")
-    # print(tbody)
-
     a_tags = tbody.find_all('a')
-
-    # Lägger till bas url:n till listan för enkelhetens skull
     base_url = "https://personal.trappan.nu/index.php"
     hrefs = [base_url + a.get('href') for a in a_tags]
 
@@ -128,16 +138,8 @@ try:
         email = driver.find_element(By.XPATH,'//*[@id="content"]/div[1]/div[2]/div[1]/div/div[2]/table/tbody/tr[1]/td[2]').text
         list_of_emails.append(email)
         
-    print(list_of_emails)
+    print(list_of_emails) # Sorterade i ingående ordning från den genererade förmånslistan på portalen
 
-   
-
-
-
-    
-
-    
-   
 
 finally:
     # Avsluta webbläsarsessionen
